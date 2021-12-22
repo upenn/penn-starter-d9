e02065093268dc411f52ff25ddae705937de9a09
Rick Ward <rickward@upenn.edu>
2021-12-22 14:01:10 -0500

    updating saml config scripts

---
+++ scripts/saml_config.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ ! -L "vendor/simplesamlphp/simplesamlphp/config" ]; then
+  if [ -d "vendor/simplesamlphp/simplesamlphp/config" ]; then
+    echo "Removing default simplesaml config directory"
+    rm -rf vendor/simplesamlphp/simplesamlphp/config
+  fi
+  echo "Re-linking simplesaml config directory"
+  ln -sf ../../../private/simplesamlphp/config.php vendor/simplesamlphp/simplesamlphp/config
+fi
+
+if [ ! -L "vendor/simplesamlphp/simplesamlphp/cert" ]; then
+ if [ -d "vendor/simplesamlphp/simplesamlphp/cert" ]; then
+  echo "Removing default simplesaml cert directory"
+  rm -rf vendor/simplesamlphp/simplesamlphp/cert
+  fi
+  echo "Re-linking simplesaml cert directory"
+  ln -s ../../../private/simplesamlphp/cert vendor/simplesamlphp/simplesamlphp/cert
+fi
+
+if [ ! -L "vendor/simplesamlphp/simplesamlphp/metadata" ]; then
+ if [ -d "vendor/simplesamlphp/simplesamlphp/metadata" ]; then
+  echo "Removing default simplesaml metadata directory"
+  rm -rf vendor/simplesamlphp/simplesamlphp/metadata
+  fi
+  echo "Re-linking simplesaml metadata directory"
+  ln -s ../../../private/simplesamlphp/metadata vendor/simplesamlphp/simplesamlphp/metadata
+fi
+
+if [ ! -L "web/simplesaml" ]; then
+ if [ -d "vendor/simplesamlphp/simplesamlphp/www" ]; then
+  echo "Linking simplesaml www directory"
+  ln -s ../vendor/simplesamlphp/simplesamlphp/www web/simplesaml
+  fi
+fi
